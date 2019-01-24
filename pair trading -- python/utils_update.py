edb42d36338a23fd122b9defd51f6f006f82b8b9
kite8 <996845018@qq.com>
2019-01-25 00:19:28 +0800

    add

--- pair trading -- python/utils_update.py
+++ pair trading -- python/utils_update.py
@@ -18,7 +18,7 @@ def update(start='20100101'):
 
     df_code = pd.read_excel('data/etf_pair_code.xlsx', dtype={'symbol':str})
     code_list = df_code.symbol.unique().tolist()
-    ticker_list = (df_code.symbol + ' ' + df_code.region).fillna('ita').unique().tolist()
+    ticker_list = (df_code.symbol + ' ' + df_code.region).unique().tolist()
 
     fields = ['HIGH', 'OPEN', 'LOW', 'VOLUME', 'PX_LAST']
     # cols = ['High', 'Open', 'Low', 'Volume', 'Adj Close']
@@ -28,7 +28,7 @@ def update(start='20100101'):
     end = datetime.datetime.today().strftime('%Y%m%d')
 
     ticker_list.remove('HKD Curncy')
-    ticker_list.remove('ita')
+    #ticker_list.remove('ita')
     intervals = np.linspace(0, len(ticker_list), 10, dtype=int)
     tic = time.perf_counter()
 
